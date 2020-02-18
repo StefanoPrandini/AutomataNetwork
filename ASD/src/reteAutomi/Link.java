764ea42459ec3a74de80ad6be5e51c298d44d931
Stefano Prandini <s.prandini003@studenti.unibs.it>
2020-02-18 17:53:40 +0100

    main

--- ASD/src/reteAutomi/Link.java
+++ ASD/src/reteAutomi/Link.java
@@ -3,10 +3,12 @@ package reteAutomi;
 public class Link {
 	Automa automaIniziale;
 	Automa automaFinale;
+	String evento;
 	
 	public Link(Automa automaIniziale, Automa automaFinale) {
 		this.automaIniziale = automaIniziale;
 		this.automaFinale = automaFinale;
+		evento = null;
 	}
 
 }
