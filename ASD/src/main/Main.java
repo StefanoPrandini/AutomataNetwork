7c6eae50008a1e04d4998fa011697eec82362aee
liviobertoni <leme95@hotmail.it>
2020-05-07 23:24:19 +0200

    prova intellij

--- ASD/src/main/Main.java
+++ ASD/src/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ciao stephen from vscode");
+		System.out.println("ciao stephen from intellij");
 		
 	}
 
