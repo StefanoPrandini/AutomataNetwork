bebe62ad5cbdbde1424c21855e7b844bb26f4d82
Stefano <s.prandini003@studenti.unibs.it>
2020-06-02 19:01:21 +0200

    inizio SpazioRilevanza2

---
+++ ASD/src/reteAutomi/SpazioRilevanza2.java
@@ -0,0 +1,63 @@
+package reteAutomi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+/**
+ * Spazio di rilevanza è un automa -> grafo: StatiRilevanza sono i vertici e transizioni sono gli archi
+ */
+public class SpazioRilevanza2 {
+	
+	private List<Automa>automi;
+	private List<Link>links;
+	
+	private Map<StatoRilevanzaReteAutomi, List<Transizione>> mappaStatoRilevanzaTransizioni;
+	
+	public SpazioRilevanza2(List<Automa>automi, List<Link>links) {
+		this.automi = automi;
+		this.links = links;
+		this.mappaStatoRilevanzaTransizioni = new HashMap<>();
+	}
+	
+	public SpazioRilevanza2 creaSpazioRilevanza() {
+		Queue<StatoRilevanzaReteAutomi> coda = new LinkedList<>();
+		StatoRilevanzaReteAutomi statoIniziale = getStatoRilevanzaIniziale();
+		coda.add(statoIniziale);
+		
+		while(!coda.isEmpty()) {
+			StatoRilevanzaReteAutomi stato = coda.remove();
+			ArrayList<Transizione>transizioniAbilitate = getTransizioniAbilitate(stato);
+			this.mappaStatoRilevanzaTransizioni.put(stato, transizioniAbilitate);
+
+			for(StatoRilevanzaReteAutomi s : getStatiRilevanzaSuccessivi(stato)) {
+				
+			}
+
+		}
+		
+		ArrayList<Transizione>transizioniAbilitate = getTransizioniAbilitate(stato);
+
+		while(transizioniAbilitate.size() > 0 ) {
+			ArrayList<StatoRilevanzaReteAutomi>statiRilevanzaSuccessivi = getStatiRilevanzaSuccessivi();
+		}
+		
+		
+		
+		return this;
+	}
+	
+	private StatoRilevanzaReteAutomi getStatoRilevanzaIniziale() {
+		ArrayList<Stato>statiIniziali = new ArrayList<>();
+		for(Automa a : this.automi) {
+			statiIniziali.add(a.getStatoIniziale());
+		}
+		ArrayList<Evento>eventiLink = new ArrayList<>();
+		ArrayList<String>decorazione = new ArrayList<>();
+		
+		return new StatoRilevanzaReteAutomi("", statiIniziali, new ArrayList<Evento>(), decorazione);
+	}
+	
+}
